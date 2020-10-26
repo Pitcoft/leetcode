5d934fc91acdd12b05a661bdbf4cd28afbd132fe
pitcoft <1006323903@qq.com>
2020-10-26 23:55:25 +0800

    coding update

---
+++ leetcode-19-删除链表的倒数第N个节点(python)/leetcode_19.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0)
+        dummy.next = head
+        slow, fast = dummy, dummy
+        for i in range(n):
+            fast = fast.next
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return dummy.next
+
+if __name__ == '__main__':
+    print(Solution().removeNthFromEnd([1,2,3,4,5],2))
