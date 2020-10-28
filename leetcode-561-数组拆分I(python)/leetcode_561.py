563de25503bdf100f8d24eae4c5b411cfaa73edc
pitcoft <1006323903@qq.com>
2020-10-28 23:57:23 +0800

    update

--- leetcode-561-数组拆分I(python)/leetcode_561.py
+++ leetcode-561-数组拆分I(python)/leetcode_561.py
@@ -1,6 +1,17 @@
 class Solution:
     def arrayPairSum(self, nums) -> int:
-        return sum(sorted(nums)[::2])
+        sum = 0
+        nums.sort()
+        for i,item in enumerate(nums):
+            if i%2 == 0: # 取下标为偶数的值
+                sum += item # 求和
+        return sum
+
+        '''
+        精简写法：
+        return sum(sorted(nums)[::2]) # 对数组排序，隔2取一个数并求和
+        '''
+
 
 if __name__ == '__main__':
     print(Solution().arrayPairSum([1,4,3,2]))
